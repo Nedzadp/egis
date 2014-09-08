712033ae5e9266826053845a286b2a209d1c56d7
Nedzad Paradzik <paradzik.nedzad@gmail.com>
2014-09-08 15:00:40 +0200

    finished tasks for help menu

---
+++ PP4Is/src/main/java/com/pss/pp4is/layout/content/views/help/MainContentListsLayout.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.pss.pp4is.layout.content.views.help;
+
+import com.pss.pp4is.layout.content.CustomVerticalLayout;
+import com.vaadin.ui.CustomLayout;
+
+/**
+ *
+ * @author Nedzad
+ */
+public class MainContentListsLayout extends CustomVerticalLayout{
+
+    public MainContentListsLayout() {
+        initLayout();
+    }
+    
+    private void initLayout() {
+        setMargin(false);
+        setSpacing(false);
+        
+        addComponent(new CustomLayout("lists"));
+    }
+}
